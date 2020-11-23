b1e32c5120f130b4dec327129dca2919357769c0
Martin Schmidt <mmartin.schmidt@gmail.com>
2020-11-23 12:24:30 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,9 +1,11 @@
 package model.dao;
 
+import db.DB;
+
 public class DaoFactory {
 	
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 		
 	}
 
