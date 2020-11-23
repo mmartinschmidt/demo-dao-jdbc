9ca45e32bdff126fe6d18c6043d565f482787f25
Martin Schmidt <mmartin.schmidt@gmail.com>
2020-11-23 19:51:22 -0300

    DepartmentDao implementation and test

---
+++ src/application/Program2.java
@@ -0,0 +1,52 @@
+package application;
+
+import java.util.List;
+import java.util.Scanner;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+        //////////////////////////////////////////////////////////////
+		System.out.println("==== Teste 1: findById ====");
+		Department dep = departmentDao.findById(1);
+		System.out.println(dep);
+        //////////////////////////////////////////////////////////////
+		System.out.println("\n==== Teste 2: findAll ====");
+		List<Department> list = departmentDao.findAll();
+		for (Department d : list) {
+			System.out.println(d);
+		}
+        //////////////////////////////////////////////////////////////
+		System.out.println("\n==== Teste 3: insert ====");
+		Department newDepartment = new Department(null, "Music");
+		departmentDao.insert(newDepartment);
+		//System.out.println("Inserted! New Id = " + newDepartment.getId());
+		System.out.println("Inserido! Novo Id = " + newDepartment.getId());
+        //////////////////////////////////////////////////////////////
+		System.out.println("\n==== Teste 4: update ====");
+		Department dep2 = departmentDao.findById(1);
+		dep2.setName("Food");
+		departmentDao.update(dep2);
+		//System.out.println("Update completed");
+        System.out.println("Update concluído");
+        //////////////////////////////////////////////////////////////
+		System.out.println("\n==== Teste 5: delete ====");
+		//System.out.print("Enter id for delete test: ");
+		System.out.println("Digite id para o teste delete: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+        //System.out.println("Delete completed");
+        System.out.println("Delete concluído");
+		sc.close();
+        //////////////////////////////////////////////////////////////		
+	}
+}
